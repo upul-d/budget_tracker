8b41e39c2d4e3eada6106ef834bb259d5217206b
upuld <u.dissanayaka@gmail.com>
2017-06-19 00:51:50 +0100

    added: create a new transaction

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -9,3 +9,15 @@ get "/transactions" do
   @transactions = Transaction.all()
   erb(:"transactions/index")
 end
+
+get "/transactions/new" do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  erb(:"transactions/new")
+end
+
+post "/transactions/new" do
+  @transaction = Transaction.new(params)
+  @transaction.save()
+  erb(:"transactions/create")
+end
